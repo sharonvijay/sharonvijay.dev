af27e82198d172b70c933dd9ff86d05fa4f7218e
sharonvijay <94788632+sharonvijay@users.noreply.github.com>
2023-04-22 16:30:28 +0530

    personal website

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const path = require("path");
 
 // server used to send send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
+app.use(express.static(path.join(__dirname, "build")));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
